9f2db8ce92c5887689cb690f78560e6cc90f5529
少轻狂 <1677568218@qq.com>
2022-08-02 19:56:18 +0800

    feat: rename preview

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import preview from './components/preview.vue'
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <preview msg="Hello Vue 3 + Vite + Strapi" />
 </template>
 
 <style>
